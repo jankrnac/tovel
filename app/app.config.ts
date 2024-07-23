52deff6a08332a472e304f75525c55549923b206
TorchSK <gtorch@gmail.com>
2024-07-23 14:11:54 +0200

    dd

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,6 @@
 // app.config.ts
 export default defineAppConfig({
     appName: "JK Eshop",
-    dataSource: 'medusa', 		//[api, content]
     maxWidth: '1900px',
     nuxtIcon: {
       	size: '24px', 			// default <Icon> size applied
